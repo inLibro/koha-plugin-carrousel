29561898b2d3d3aadf7c11af081a153c96b7335c
charles <charles.farmer@inlibro.com>
2014-08-01 13:45:07 -0400

    instauto #841: Le plugin offre maintenant la possibilité de restaurer une bd, d'enregistrer localement un backup et d'effectuer un manuellement un backup.
    	modified:   Sauvegardes.pm
    	modified:   Sauvegardes/sauvegardes.tt

--- plugins/Sauvegardes.pm
+++ plugins/Sauvegardes.pm
@@ -1,7 +1,7 @@
 package Koha::Plugin::Sauvegardes;
 
 use Modern::Perl;
-use File::Basename;
+use File::Spec;
 use base qw(Koha::Plugins::Base);
 use C4::Context;
 use C4::Auth;
@@ -32,22 +32,31 @@ sub tool {
     my $cgi = $self->{'cgi'};
     my $taskId = $cgi->param('taskid');
     my $backup = $cgi->param('backup');
+    my $request = $cgi->param('submitbuttontype');
+    my $statusupdate = $cgi->param('status');
     
     my %params;
     
-    if ($taskId) { # we're looking for a status
+    if ($taskId && ($statusupdate eq 'WAITING')) { # we're looking for a status
         my ($status, $log) = status($taskId);
         $params{'log'} = $log;
         $params{status} = $status;
-    } elsif ($backup) {
-#        die $cgi->param('submitbuttontype');
-        my ($id, $status, $log) = downloadBackup($backup); #applyBackup($backup);
+    } elsif ($request eq "Download" && $backup) {
+        downloadBackup($backup);
+    } elsif ($request eq "Install" && $backup) {
+        my ($id, $status, $log) = applyBackup($backup);
+        $params{'log'} = $log;
+        $params{'status'} = $status;
+        $taskId = $id;
+    } elsif ($request eq "Backup"){
+        my ($id, $status, $log) = saveBackup($backup);
         $params{'log'} = $log;
         $params{'status'} = $status;
         $taskId = $id;
     }
     
     $params{taskid} = $taskId;
+    $params{request} = $request;
     $params{backups} = listBackups();
 
     my $template = $self->get_template({ file => 'sauvegardes.tt' });
@@ -59,58 +68,87 @@ sub tool {
 
 sub listBackups {
     my ( $client ) = grep { s/koha_// && s/_.*_.*// } C4::Context->config('database');
+    my $backupDir = "/inlibro/backups/db";
     
-    opendir(my $backupdir, "/inlibro/backups/db/2907") or ( warn "failed to opendir\n" and return ); #opendir(my $backupdir, "/inlibro/backups/db/$client") or ( warn "failed to opendir\n", return );
-    my @backuplist = reverse grep { s/\.sql\.gz// && s/.*?-// } readdir ($backupdir);
-    closedir($backupdir);
-    
-    return \@backuplist;
+    opendir(my $dh, "$backupDir/$client") or ( warn "failed to opendir $backupDir/$client\n", return );
+    my @backuplist = grep { s/\.sql\.gz// && s/.*?-// } readdir ($dh);
+    my $l='';
+    my @a;
+    foreach (@backuplist) {
+        my $annee = substr $_, 0, 4;
+        my $mois  = substr $_, 4, 2;
+        my $jour  = substr $_, 6, 2;
+        my $heure = substr $_, 9;
+        if(index($heure, ":") == -1){
+            my @t = ( $heure =~ m/../g );
+            $heure = join (":", @t);
+        }
+        push @a, "$jour/$mois/$annee, $heure";
+    }
+    closedir($dh);
+    @a = map  $_->[0],
+         sort { $a->[1] cmp $b->[1] }
+         map  [ $_, join('', (split '/', $_)[1,0]) ], @a; # magie noire pour trier dates
+    @a = reverse @a;
+    return \@a;
 }
 
 sub applyBackup {
-    my $backupChoisi = trim( shift );
+    my $backupChoisi = parseDate(trim( shift ));
+    my $backupDir = "/inlibro/backups/db";
     my $clientdb = C4::Context->config('database');
     my ( $client ) = grep { s/koha_// && s/_.*_.*// } C4::Context->config('database');
     
-    opendir(my $dh, "/inlibro/backups/db/2907") or ( warn "failed to opendir\n" and return ); #opendir(my $dh, "/inlibro/backups/db/$client") or ( warn "failed to opendir\n", return );
+    opendir(my $dh, "$backupDir/$client") or ( warn "failed to opendir $backupDir/$client\n", return );
     ( $backupChoisi ) = grep { /$backupChoisi/ } readdir ($dh);
     closedir($dh);
     
     my $tasker = Koha::Tasks->new();
-    my $taskId = $tasker->addTask(name =>"PLUGIN-MANAGEBACKUPS", command=>"gunzip -c $backupChoisi | mysql -u -p $clientdb");
-    
+    my ( $u, $p ) = ( C4::Context->config('user'), C4::Context->config('pass') );
+    my $taskId = $tasker->addTask(name =>"PLUGIN-MANAGEBACKUPS", command=>"gunzip -c $backupChoisi | mysql -u$u -p$p $clientdb");
     for (my $i = 0; $i < 10; $i++){
         sleep 3;
         my $task = $tasker->getTask($taskId);
-        return ($task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILED' ); 
+        return ($task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILURE' ); 
     }
     return $taskId;
 }
 
 sub downloadBackup {
-    my $backupChoisi = "/inlibro/backups/db/2907/machin"; #trim( shift );
-    opendir(my $dh, "/inlibro/backups/db/2907") or ( warn "failed to opendir\n" and return ); #opendir(my $dh, "/inlibro/backups/db/$client") or ( warn "failed to opendir\n", return );
-#    ( $backupChoisi ) = grep { /$backupChoisi/ } readdir ($dh);
+    my $backupChoisi = parseDate(trim( shift ));
+    my $backupDir = "/inlibro/backups/db";
+    my ( $client ) = grep { s/koha_// && s/_.*_.*// } C4::Context->config('database');
+    
+    opendir(my $dh, "$backupDir/$client") or ( warn "failed to opendir $backupDir/$client\n", return );
+    ( $backupChoisi ) = grep { /$backupChoisi/ } readdir ($dh);
+    return if !$backupChoisi;
     closedir($dh);
-    
-    #
-    #
-    # FAIRE TRÈS ATTENTION AU GREP CI-HAUT
-    #
-    
-    my $filename = "/inlibro/backups/db/2907/$backupChoisi";
-    my ( $file, $dirs, $suffix ) = fileparse ($filename);
-    die $file."\n".$dirs."\n".$suffix;
-    return if $dirs != "/inlibro/backups/db/2907";
+
+    my $filename = "$backupDir/$client/$backupChoisi";
+    my ( $volume,$directories,$file ) = File::Spec->splitpath ($filename);
+    return if $directories != "$backupDir/$client";
     
     open(FILE, "<", "$filename") or ( warn "failed to open file $filename\n" and return );
     my @fileholder = <FILE>;
     close(FILE);
-    
     $filename =~ s/.*\///;
     print "Content-Type:application/x-download\n";
     print "Content-Disposition:attachment;filename=$filename\n\n";
     print @fileholder;
+}
+
+sub saveBackup {
+    my $tasker = Koha::Tasks->new();
+    my $clientdb = C4::Context->config('database');
+    my ( $client ) = grep { s/koha_// && s/_.*_.*// } C4::Context->config('database');    
+    my $backupName = "$clientdb-" . trim( `date +\%Y\%m\%d-\%H\%M\%S` ) . ".sql.gz"; 
+    my $taskId = $tasker->addTask(name =>"PLUGIN-MANAGEBACKUPS", command=>"mysqldump -uinlibrodumper -pinlibrodumper $clientdb | gzip -c -9 > /inlibro/backups/db/$client/$backupName");
+    for (my $i = 0; $i < 10; $i++){
+        sleep 3;
+        my $task = $tasker->getTask($taskId);
+        return ($task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILURE' ); 
+    }
+    return $taskId;
 }
 
 sub status {
@@ -119,6 +157,19 @@ sub status {
     return "Internal error, unknown task id $taskId" unless $hrTask;
     
     return ($hrTask->{status}, $hrTask->{'log'});
+}
+
+sub parseDate {
+    #
+    # risque de changer en fonction des specs de sauvegarde
+    # 
+    my $d = shift; 
+    my ( $date, $heure ) = split ( ", ", $d );
+    my ( $annee, $mois, $jour ) = split ( "/", $date );
+    if ( substr($heure, 0, 2) gt "03" ){
+        $heure =~ s/://g;
+    }
+    return ($jour.$mois.$annee."-".$heure);
 }
 
 sub install() {
