1d923f36bbe6b4304ae7402ff5ccc55c7bfc12c1
charles <charles.farmer@inlibro.com>
2014-08-13 11:17:11 -0400

    instauto #957: updateTask, cancelTask et Reindex devrait tous fonctionner. À tester...

--- plugins/Reindexing.pm
+++ plugins/Reindexing.pm
@@ -45,37 +45,109 @@ sub tool {
         'zebratbl'   => $cgi->param('group') eq "zebratbl"  ? 'on' : undef,
         'reset'      => $cgi->param('reset')     || undef,
         'email'      => $cgi->param('email')     || undef,
-        'email2'     => $cgi->param('email2')    || undef,
+        'emailaddr'     => $cgi->param('emailaddr')    || undef,
         'startlater' => $cgi->param('startlater')|| undef,
         'hour'       => $cgi->param('hour')      || undef,
         'minute'     => $cgi->param('minute')    || undef,
     );
-    
+    #
+    # Switch vers les opérations
+    #
     if ($canceltask){
         my $nbrow = cancelTask();
-        $params{status} = 'DELETED' if ( $nbrow > 0 );
+        $params{status} = $nbrow > 0  ? 'DELETED' : 'ERROR';
     } elsif ($updatenow){
-        
+        my ($nbrow, $id, $status, $log) = updateTask( %list );
+        $params{status} = $nbrow > 0  ? 'UPDATED' : 'ERROR';
+        $params{'log'} = $log if $log;
+        $params{'reindexstatus'} = $status if $status;
+        abort("ERROR: the reindexing process did not complete in time and could still be running.") if ($id && !$status);
     } elsif ($reindexnow) {
         my ($id, $status, $log) = reindex_zebra( %list );
         $params{'log'} = $log;
-        $params{'status'} = $status;
+        $params{'reindexstatus'} = $status;
         abort("ERROR: the reindexing process did not complete in time and could still be running.") unless $status;
     }
     
+    $params{reindexnow} = '';
     $params{updatenow}  = '';
     $params{canceltask} = '';
-    $params{reindexnow} = '';
-    $params{email2} = $list{'email2'} || C4::Context->preference('KohaAdminEmailAddress');
-    ($params{status}, $params{timenext}) = getWaiting() if ($params{status} ne 'FAILURE' && $params{status} ne 'COMPLETED' && $params{status} ne 'DELETED');
+    $params{emailaddr} = $list{'emailaddr'} || C4::Context->preference('KohaAdminEmailAddress');
     
+    my $th = getWaiting();# unless ($params{status} =~ /(FAILURE|COMPLETED|DELETED)/ );
+    ( $params{nexttaskstatus}, $params{timenext} ) = ($th->{(keys $th)[0]}->{status}, $th->{(keys $th)[0]}->{time_next}) if ($th);
+    
+    $params{mailstatus} = sendEmail($params{emailaddr}, 'pluginzebra@inlibro.com', "$params{'reindexstatus'}\n\n$params{log}") if ($list{email} && $params{'reindexstatus'});
+        
     my $template = $self->get_template({ file => 'reindexing.tt' });
     $template->param( %params );
     print $cgi->header();
     print $template->output();
 }
 
-sub reindex_zebra{
+sub reindex_zebra {
+    my %list = @_;
+    my ($command, $timestring) = buildQuery( %list );
+    my $tasker = Koha::Tasks->new();
+    my $taskId = $tasker->addTask(name =>"PLUGIN-REBUILDZEBRA", command=>"$command", time_next=>$timestring);
+    for (my $i = 0; $i < 20; $i++){
+        sleep 3;
+        my $task = $tasker->getTask($taskId);
+        return ($task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILURE' || ($task->{status} eq 'WAITING' && $timestring) ); 
+    }
+    return $taskId;
+}
+
+sub cancelTask {
+    my $tasker = Koha::Tasks->new();
+    my $intranetdir = C4::Context->config("intranetdir");
+    my $command = "cd $intranetdir; ./misc/migration_tools/rebuild_zebra.pl";
+    my $th = $tasker->getTasksRegexp(command=>$command, status=>'WAITING');
+    return $tasker->deleteTask((keys $th)[0]);
+}
+
+sub updateTask {
+    my %list = @_;
+    my ($command, $timestring) = buildQuery( %list );
+    my $th = getWaiting();
+
+    return -1 unless $th;
+    
+    my $taskId = (keys $th)[0];
+    my %args = (
+        id          => $taskId,
+        command     => $command,
+        time_next   => $timestring,
+    ); 
+    my $tasker = Koha::Tasks->new();
+    my $nbrow = $tasker->update( %args );
+    
+    return $nbrow if $timestring;
+    
+    for (my $i = 0; $i < 20; $i++){
+        sleep 3;
+        my $task = $tasker->getTask($taskId);
+        return ($nbrow, $task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILURE' || ($task->{status} eq 'WAITING' && $timestring) ); 
+    }
+    return ($nbrow, $taskId);
+    
+}
+
+sub getWaiting {
+    my $tasker = Koha::Tasks->new();
+    my $intranetdir = C4::Context->config("intranetdir");
+    my $command = "cd $intranetdir; ./misc/migration_tools/rebuild_zebra.pl";
+    my $th = $tasker->getTasksRegexp(command=>$command, status=>'WAITING');
+#    if($th){
+#        my ($id) = keys $th;
+#        my $time = Time::Piece->strptime($th->{$id}->{time_next}, "%Y-%m-%d %H:%M:%S" );
+#        return ($th->{$id}->{status}, $time->strftime("%c"));
+#    }
+#    return (undef, undef);
+    return (%{$th} ? $th : undef);
+}
+
+sub buildQuery {
     my %list = @_;
     my $intranetdir = C4::Context->config("intranetdir");
     my $command = "cd $intranetdir; ./misc/migration_tools/rebuild_zebra.pl";
@@ -94,35 +166,23 @@ sub reindex_zebra{
         
         $timestring = $u->strftime("%Y-%m-%d %H:%M:%S");
     }
-    my $tasker = Koha::Tasks->new();
-    my $taskId = $tasker->addTask(name =>"PLUGIN-REBUILDZEBRA", command=>"$command", time_next=>($timestring||"0"));
-    for (my $i = 0; $i < 20; $i++){
-        sleep 3;
-        my $task = $tasker->getTask($taskId);
-        return ($task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILURE' || ($task->{status} eq 'WAITING' && $timestring) ); 
-    }
-    return $taskId;
+    return ($command, $timestring);
 }
 
-sub getWaiting {
-    my $tasker = Koha::Tasks->new();
-    my $intranetdir = C4::Context->config("intranetdir");
-    my $command = "cd $intranetdir; ./misc/migration_tools/rebuild_zebra.pl";
-    my $th = $tasker->getTasksRegexp(command=>$command, status=>'WAITING');
-    if($th){
-        my ($id) = keys $th;
-        my $time = Time::Piece->strptime($th->{$id}->{time_next}, "%Y-%m-%d %H:%M:%S" );
-        return ($th->{$id}->{status}, $time->strftime("%c"));
-    }
-    return (undef, undef);
-}
+sub sendEmail {
+    my $to = join ("\@", split ("@", shift));
+    my $from = shift;
+    my $message = shift;
+    open MAIL,"|-","/usr/lib/sendmail","-ti";
+    print MAIL <<EOF;
+To: $to
+From: $from
+Subject: Zabbix's Rebuild Status
 
-sub cancelTask() {
-    my $tasker = Koha::Tasks->new();
-    my $intranetdir = C4::Context->config("intranetdir");
-    my $command = "cd $intranetdir; ./misc/migration_tools/rebuild_zebra.pl";
-    my $th = $tasker->getTasksRegexp(command=>$command, status=>'WAITING');
-    return $tasker->deleteTask((keys $th)[0]);
+Your latest Zebra rebuild task finished with the following status: $message
+EOF
+    close MAIL;
+    return 1;
 }
 
 sub status {
@@ -133,19 +193,19 @@ sub status {
     return ($hrTask->{status}, $hrTask->{'log'});
 }
 
-sub install() {
+sub install {
     my ( $self, $args ) = @_;
     return 1; # succès (0 pour erreur?)
 }
 
-sub uninstall() {
+sub uninstall {
     my ( $self, $args ) = @_;
     return 1; # succès
 }
 
 sub abort {
     $params{'log'} = shift;
-    $params{'status'} = 'FAILURE';
+    $params{'reindexstatus'} = 'FAILURE';
 }
 
 1;
