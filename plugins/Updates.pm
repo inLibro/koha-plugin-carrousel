2aa2ae97bb0255927ea049f7193036ea47798aef
charles <charles.farmer@inlibro.com>
2014-08-20 16:40:37 -0400

    instauto #839: division des commandes pour un meilleur suivi. améliorations possibles quant à l'affichage du statut entre les différentes étapes (backup, checkout,updatedatabase, inst. de langues)

--- plugins/Updates.pm
+++ plugins/Updates.pm
@@ -40,65 +40,98 @@ sub tool {
         $params{'log'} = $log;
         $params{status} = $status;
     } elsif ($version) {
-        my ($id, $status, $log) = installVersion($version);
-        warn "RETOUR À UPDATES.PM APRÈS L'EXÉCUTION DE INSTALLVERSION";
-        $params{'log'} = $log;
-        $params{'status'} = $status;
-        $taskId = $id;
-        abort("ERROR: the installation process did not complete in time and could still be running.") unless $status;
+        my $return = launchUpdateSequence($version);
+        $params{'return'} = $return;
     }
+    
+    if ( defined $params{'return'} && $params{'return'} != -1 ){
+        print $cgi->redirect("/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Updates&method=tool&status=1&logout.x=1");
+    }
+    
     $params{taskid} = $taskId;
     $params{version} = $version;
-    warn "AVANT C4::CONTEXT::KOHAVERSION";
-    $params{'kohaVersion'} = C4::Context::KOHAVERSION;
-    $params{'versions'} = trouverVersion($params{'kohaVersion'});
-    
+    $params{versions} = trouverVersion();
     my $template = $self->get_template({ file => 'updates.tt' });
-    $template->param( %params );
-    warn "AVANT C4::CONTEXT->PREFERENCE";
-    my $prefversion = C4::Context->preference('Version');
-    warn "------------\n$prefversion\n$params{kohaVersion}\n$version\n------------\n";
-    
-    if($params{'status'} eq 'COMPLETED'){
-        print $cgi->redirect("/cgi-bin/koha/mainpage.pl?logout.x=1");        
-    }
-    
+    $template->param( %params );    
     print $cgi->header();
     print $template->output();
 }
 
-sub installVersion {
-    my $v = trim("v" . shift);
-    my $intranetdir  = C4::Context->config("intranetdir");
-    my $translatedir = C4::Context->config("intranetdir")."/misc/translator";
-    my $command = "cd $intranetdir; ";
-    $command .= "git checkout -f $v; ";
-    $command .= "./installer/data/mysql/updatedatabase.pl; ";
-    $command .= "cd $translatedir; ";
-    my @installed = map { $_->{rfc4646_subtag} } @{C4::Languages::getTranslatedLanguages()};
-    foreach(@installed){
-        # we lose our installed languages with git checkout -f, so we install them back
-        $command .= "./translate install $_; ";
-    }
+sub launchUpdateSequence {
+    my $version = shift;
+    my ($id, $status, $log);
+    #lance le backup
+    ($id, $status, $log) = preemptiveBackup();
+    return -1 unless ($status || $status eq 'COMPLETED');
+    #lance le checkout, l'update
+    ($id, $status, $log) = installVersion($version);
+    return -1 unless ($status || $status eq 'COMPLETED');
+    #lance l'installation des langues
+    ($id, $status, $log) = installLang();
+    return -1 unless ($status || $status eq 'COMPLETED');
     
+    return 0;
+}
+
+sub launchTask {
+    my $name = shift;
+    my $command = shift;
     my $tasker = Koha::Tasks->new();
     my $taskId = $tasker->addTask(
-        name        => "PLUGIN-VERSIONUPDATE",
+        name        => $name,
         command     => $command
     );
     for (my $i = 0; $i < 30; $i++){
         sleep 3;
         my $task = $tasker->getTask($taskId);
-        warn "EXECUTION DE LA MISE À JOUR";
         return ($task->{id}, $task->{status}, $task->{log}) if ( $task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILURE' ); 
     }
     return $taskId;
 }
 
+sub installVersion {
+    my $v = trim("v" . shift);
+    my $intranetdir = C4::Context->config("intranetdir");
+    my $command = "cd $intranetdir; ";
+    $command .= "git checkout -f $v; ";
+    $command .= "./installer/data/mysql/updatedatabase.pl; ";
+    
+    return launchTask("PLUGIN-UPDATE-UPDATE", $command);
+}
+
+sub preemptiveBackup {
+    my $tasker = Koha::Tasks->new();
+    my $clientdb = C4::Context->config('database');
+    my ( $client ) = grep { s/koha_// && s/_.*_.*// } C4::Context->config('database');    
+    my $backupName = $clientdb . "-" . trim( `date +\%Y\%m\%d-\%H\%M\%S` ) . "-MANUAL.sql.gz";
+    my $backupDir = "/inlibro/backups/db";
+    my $command = "mysqldump -uinlibrodumper -pinlibrodumper $clientdb --single-transaction --ignore-table=$clientdb.tasks | gzip -c -9 > $backupDir/$client/$backupName";
+    
+    unless (-d "$backupDir/$client"){
+        my $id = $tasker->addTask(name =>"PLUGIN-VERSIONUPDATE-CREATEDIR", command=>"mkdir $backupDir/$client");
+        sleep 1 while ( $tasker->getTask($id)->{status} ne 'COMPLETED' && $tasker->getTask($id)->{status} ne 'FAILURE' );
+    }
+    
+    return launchTask("PLUGIN-UPDATE-BACKUP", $command);
+}
+
+sub installLang {
+    my $translatedir = C4::Context->config("intranetdir")."/misc/translator";
+    my $command = "cd $translatedir; ";
+    my @installed = map { $_->{rfc4646_subtag} } @{C4::Languages::getTranslatedLanguages()};
+    $params{'languages'} = \@installed;
+    foreach(@installed){
+        # we lose our installed languages with git checkout -f, so we install them back
+        $command .= "./translate install $_; ";
+    }
+    
+    return launchTask("PLUGIN-UPDATE-LANG", $command);
+}
+
 sub trouverVersion() {
     my $dir = C4::Context->config("intranetdir");
     chdir($dir) or ( abort("ERROR: failed to reach your installation directory.") and return );
-    my ( $cutoff_major, $cutoff_functional, $cutoff_subnumber ) = split ( /\./, shift );
+    my ( $cutoff_major, $cutoff_functional, $cutoff_subnumber ) = split ( /\./, C4::Context::KOHAVERSION );
     my @versionlist = reverse grep { $_ =~ /^v.*\.[0-9]{2}$/ && s/v//g } qx( git tag );
     
     my @arr;
