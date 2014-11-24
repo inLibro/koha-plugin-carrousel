3fa4324acca042cedb4c42502b9a57ff6f412d4b
ppp <pierre-paul.paquin@inlibro.com>
2014-11-24 12:59:28 -0500

    améliore le plugin d'installation de langues
     - on a une indication de progrès pendant qu'une langue installe
     - on peut "refresh" la page sans rien briser
     - on peut choisir dans quelle interface les langues sont disponibles
     - on peut installer/activer plusieurs variantes de la même langue
    
    Voir http://camelot.babylone.inlibro.net/issues/905
      et http://camelot.babylone.inlibro.net/issues/882

--- plugins/Languages.pm
+++ plugins/Languages.pm
@@ -7,6 +7,7 @@ use C4::Context;
 use C4::Auth;
 use C4::Languages;
 use Koha::Tasks;
+use JSON;
 
 sub new {
     my ( $class, $args ) = @_;
@@ -29,50 +30,93 @@ sub new {
 
 sub tool {
     my ( $self, $args ) = @_;
-
     my $cgi = $self->{'cgi'};
-    my $language = $cgi->param('language');
-    my $taskId = $cgi->param('taskid');
+    binmode(STDOUT, ':utf8');
     my %params;
-    if($taskId){ # we're looking for a status
-        my ($status, $log) = status($taskId);
-#        $taskId = 0 if(! $status =~ /WAITING|PROCESSING/);
-        $params{'log'} = $log;
-        $params{status} = $status;
-    }elsif($language){
-        my ($id, $status, $log) = installLanguage($language);
-        $params{'log'} = $log;
-        $params{status} = $status;
-        $taskId = $id;
-#        $params{selectedlanguage} = 'fr-CA';
-    } 
-#    $params{taskid} = $taskId;
-#    $params{language} = $language;
-        
+
+    # which languages are still installing
+    my %installing = ();
+    my $iq = Koha::Tasks->new()->getTasksRegexp('name' => 'PLUGIN-LANGUAGES', 'status' => 'WAITING|PROCESSING');
+    while(my($unused, $t) = each %$iq) {
+        if($t->{'command'} =~ /.\/translate install (.+)/) {
+            $installing{$1} = 1;
+        }
+    }
+
+    # GET ?status=1
+    if(uc($cgi->request_method) eq 'GET' && $cgi->param('status')) {
+        print $cgi->header(-Content_type => 'application/json');
+        print encode_json(\%installing);
+        return;
+    }
+
     # obtenir la liste des langues pour les thèmes DISPONIBLES.  La librairie C4::Languages ne vérifie que ce qui est déjà installé.
     my $dir=C4::Context->config('intranetdir')."/misc/translator/po";
     opendir (MYDIR,$dir);
     my @languages = sort map {$_ =~ /^(.*)-opac-bootstrap.po/; $1; } grep { /-opac-bootstrap.po/ } readdir(MYDIR);    
     closedir MYDIR;
-    my @installed = map { $_->{rfc4646_subtag} } @{C4::Languages::getTranslatedLanguages()};
-    foreach my $t (@installed){
-        @languages = grep { $_ ne $t} @languages;
+    my @installed = grep { !($installing{$_}) } map { map { $_->{rfc4646_subtag} } @{$_->{sublanguages_loop}} } @{C4::Languages::getTranslatedLanguages()};
+    my %installed = map { $_ => 1 } @installed;
+
+    # POST
+    # Either we're installing a new language,
+    # or we're updating the language availability settings.
+    if(uc($cgi->request_method) eq 'POST') {
+        my $new_language = $cgi->param('new_language');
+        if($new_language) {
+            installLanguage($new_language);
+        } else {
+            # We can only activate languages that are installed
+            # There MUST be at least one active language per interface;
+            # reject requests to set 0 active languages
+            for my $k ('language', 'opaclanguages') {
+                my $langs = join(',', grep { $installed{$_} } $cgi->param($k));
+                $langs and C4::Context->set_preference($k, $langs);
+            }
+        }
+        # prevent double-posting by suggesting a GET-redirect
+        print $cgi->header(-status => 303, -Location => '/cgi-bin/koha/plugins/run.pl?class=Koha%3A%3APlugin%3A%3ALanguages&method=tool');
+        return;
     }
-    
+
+    # which language is available where
+    my %intranet_languages = map { $_ => 1 } split(/,/, C4::Context->preference('language'));
+    my %opac_languages     = map { $_ => 1 } split(/,/, C4::Context->preference('opaclanguages'));
+
+    my @install_deets = ();
+    foreach my $l (@installed) {
+        push(@install_deets, {
+                'language' => $l,
+                'enabled_for_intranet' => $intranet_languages{$l},
+                'enabled_for_opac' => $opac_languages{$l},
+            });
+    }
+    while(my($l, $unused) = each %installing) {
+        push(@install_deets, {
+                'language' => $l,
+                'installing' => 1,
+            });
+    }
+
+    @languages = grep { !($installed{$_}) && !($installing{$_})} @languages;
+
+    my @installing = keys %installing;
+
     $params{languages} = \@languages;
-    $params{languagesinstalled} = \@installed;
+    $params{languagesinstalled} = \@install_deets;
+    $params{installing} = \@installing;
     
     my $template = $self->get_template({ file => 'languages.tt' });
 
     $template->param( %params );
     
-    print $cgi->header();
+    print $cgi->header(-charset => 'utf8');
     print $template->output();
 }
 
 sub installLanguage{
     my ($language) = @_;
-    
+
     # install the template
     my $translatedir = C4::Context->config('intranetdir')."/misc/translator";
     my $command = "cd $translatedir; ./translate install $language";
@@ -82,25 +126,12 @@ sub installLanguage{
     if($hrOldTasks){
         foreach my $id(keys $hrOldTasks){
             if($hrOldTasks->{$id}->{status} ne 'FAILED'){
-                return ($id,$hrOldTasks->{$id}->{status},$hrOldTasks->{$id}->{log});
+                return $id;
             }
         }
     }
     my $taskId = $tasker->addTask(name =>"PLUGIN-LANGUAGES", command=>$command);
-    
-    # add the language to the display choices
-    foreach my $display ('language','opaclanguages'){
-        my $value = C4::Context->preference($display);
-        next if $value =~ /$language/;
-        C4::Context->set_preference($display, "$value,$language");
-    }
 
-#je suis tanné de tenter de coder le progress bar "simplement", so fuck it pour l'instant
-for (my $i = 0; $i < 10; $i++){
-    sleep 3;
-    my $task = $tasker->getTask($taskId);
-    return ($task->{id}, $task->{status}, $task->{log}) if ($task->{status} eq 'COMPLETED' || $task->{status} eq 'FAILED'); 
-}
     return $taskId;
 }
 
