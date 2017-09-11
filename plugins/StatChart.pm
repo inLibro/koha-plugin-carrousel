608d246f78b3246421811f494516a6657a78ef7c
David Bourgault <david.bourgault@inlibro.com>
2017-09-11 16:24:33 -0400

    cam5081 - correctifs et nouvelle image

--- plugins/StatChart.pm
+++ plugins/StatChart.pm
@@ -1,8 +1,8 @@
 package Koha::Plugin::StatChart;
-# David Bourgault, 2017 - Inlibro
+
+# David Bourgault, 2017 - Solutions inLibro
 #
 # Generates charts from various datasets, at user's request
-#
 #
 # This file is part of Koha.
 #
@@ -27,155 +27,180 @@ use base qw(Koha::Plugins::Base);
 use C4::Auth;
 use C4::Context;
 
-our $VERSION = 0.2;
+our $VERSION  = 1.0;
 our $metadata = {
-	name            => 'StatChart',
-	author          => 'David Bourgault',
-	description     => 'Generates charts from various selectable datasets',
-	date_authored   => '2017-09-08',
-	date_updated    => '2017-09-11',
-	minimum_version => '3.20',
-	maximum_version => '17.05',
-	version         => $VERSION,
+    name            => 'StatChart',
+    author          => 'David Bourgault',
+    description     => 'Generates charts from various selectable datasets',
+    date_authored   => '2017-09-11',
+    date_updated    => '2017-09-11',
+    minimum_version => '3.20',
+    maximum_version => '17.05',
+    version         => $VERSION,
 };
 
 my @graph_presets = (
-	{ 
-		id => 'graph-loans-dow',
-		title_en => "Loans per day of the week", 
-		title_fr => "Emprunts par jour de la semaine",
-		type => "bar",
-		query => "SELECT DAYOFWEEK(issuedate), DAYNAME(issuedate), COUNT(issuedate) FROM (SELECT issuedate FROM issues UNION SELECT issuedate FROM old_issues) AS all_issues GROUP BY DAYOFWEEK(issuedate);"
-	},
-	{ 
-		id => 'graph-loans-itype',
-		title_en => "Loans per document type", 
-		title_fr => "Emprunts par type de document",
-		type => "pie",
-		query => "SELECT items.itype, itemtypes.description, COUNT(items.itype) FROM ((SELECT itemnumber FROM issues) UNION (SELECT itemnumber FROM old_issues)) as all_issues INNER JOIN items ON all_issues.itemnumber=items.itemnumber INNER JOIN itemtypes ON items.itype=itemtypes.itemtype GROUP BY items.itype ORDER BY COUNT(items.itype) DESC;"
-	},
-	{ 
-		id => 'graph-loans-year',
-		title_en => "Loans per years", 
-		title_fr => "Emprunts par année",
-		type => "bar",
-		query => "SELECT YEAR(issuedate), YEAR(issuedate), COUNT(*) FROM (SELECT issuedate FROM issues UNION SELECT issuedate FROM old_issues) AS all_issues GROUP BY YEAR(issuedate) ORDER BY YEAR(issuedate) ASC;"
-	}
+    {   id       => 'graph-loans-dow',
+        title_en => "Loans per day of the week",
+        title_fr => "Emprunts par jour de la semaine",
+        units_en => "Loans",
+        units_fr => "Emprunts",
+        type     => "bar",
+        query =>
+            "SELECT DAYOFWEEK(issuedate), DAYNAME(issuedate), COUNT(issuedate) FROM (SELECT issuedate FROM issues UNION SELECT issuedate FROM old_issues) AS all_issues GROUP BY DAYOFWEEK(issuedate);"
+    },
+    {   id       => 'graph-loans-itype',
+        title_en => "Loans per document type",
+        title_fr => "Emprunts par type de document",
+        units_en => "Loans",
+        units_fr => "Emprunts",
+        type     => "pie",
+        query =>
+            "SELECT items.itype, itemtypes.description, COUNT(items.itype) FROM ((SELECT itemnumber FROM issues) UNION (SELECT itemnumber FROM old_issues)) as all_issues INNER JOIN items ON all_issues.itemnumber=items.itemnumber INNER JOIN itemtypes ON items.itype=itemtypes.itemtype GROUP BY items.itype ORDER BY COUNT(items.itype) DESC;"
+    },
+    {   id       => 'graph-loans-year',
+        title_en => "Loans per years",
+        title_fr => "Emprunts par année",
+        units_en => "Loans",
+        units_fr => "Emprunts",
+        type     => "bar",
+        query =>
+            "SELECT YEAR(issuedate), YEAR(issuedate), COUNT(*) FROM (SELECT issuedate FROM issues UNION SELECT issuedate FROM old_issues) AS all_issues GROUP BY YEAR(issuedate) ORDER BY YEAR(issuedate) ASC;"
+    }
 );
 
 my $locale = "en_US";
+my @supportedLocales = ( 'en', 'fr' );
 
 our $dbh = C4::Context->dbh();
+
 sub new {
-	my ( $class, $args ) = @_;
-	## We need to add our metadata here so our base class can access it
-	$args->{'metadata'} = $metadata;
-	$args->{'metadata'}->{'class'} = $class;
+    my ( $class, $args ) = @_;
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
 
-	## Here, we call the 'new' method for our base class
-	## This runs some additional magic and checking
-	## and returns our actual $self
-	my $self = $class->SUPER::new($args);
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
 
-	return $self;
+    return $self;
 }
 
-
 sub tool {
-	my ( $self, $args ) = @_;
-	my $cgi = $self->{'cgi'};
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
 
-	if ($cgi->param('action')){
-		$self->graph();
-	} else {
-		$self->home();
-	}
+    if ( $cgi->param('action') ) {
+        $self->graph();
+    }
+    else {
+        $self->home();
+    }
 }
 
 sub home {
-	my ( $self, $args) = @_;
-	my $cgi = $self->{'cgi'};
-	$locale = $cgi->cookie('KohaOpacLanguage');
-	
-	my $template = $self->get_template( { file => "home.tt" } );
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+    $locale = $cgi->cookie('KohaOpacLanguage');
 
-	$template->param(locale => substr($locale, 0, 2), graph_presets => \@graph_presets);
-	print $cgi->header(-type => 'text/html',-charset => 'utf-8');
-	print $template->output();
+    my $template = $self->get_template( { file => "home.tt" } );
+    $template->param( locale => loc_code(), graph_presets => \@graph_presets );
+
+    print $cgi->header( -type => 'text/html', -charset => 'utf-8' );
+    print $template->output();
 }
 
 sub graph {
-	my ( $self, $args) = @_;
-	my $cgi = $self->{'cgi'};
-	$locale = $cgi->cookie('KohaOpacLanguage');
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+    $locale = $cgi->cookie('KohaOpacLanguage');
 
-	# Get all checked graphs
-	my @graphs;
-	for my $key ($cgi->param('graphs')) {
-		for my $preset (@graph_presets) {
-			if ($key eq $preset->{id}) {
-				push @graphs, { type=> $preset->{type}, title => $preset->{'title_' . substr($locale, 0, 2)}, data => fetch($preset->{query})};
+    # Get all checked graphs
+    my @graphs;
+    for my $key ( $cgi->param('graphs') ) {
+        for my $preset (@graph_presets) {
+            if ( $key eq $preset->{id} ) {
+                push @graphs, { type => $preset->{type}, title => $preset->{ 'title_' . loc_code() }, units => $preset->{ "units_" . loc_code() }, data => fetch( $preset->{query} ) };
 
-				# Graph with weekday as keys need some help :)
-				if ($key eq 'graph-loans-dow') {
-					$graphs[$#graphs]{data} = weekday_fixer(@{$graphs[$#graphs]{data}});
-				}
-			}
-		}
-	}
-   
-	my $template = $self->get_template( { file => "graph.tt" } );
-	
-	$template->param(locale => substr($locale, 0, 2), graphs => \@graphs);
-	print $cgi->header(-type => 'text/html',-charset => 'utf-8');
-	print $template->output();
+                # Graph with weekday as keys need some help :)
+                if ( $key eq 'graph-loans-dow' ) {
+                    $graphs[$#graphs]{data} = weekday_fixer( @{ $graphs[$#graphs]{data} } );
+                }
+            }
+        }
+    }
+
+    my $template = $self->get_template( { file => "graph.tt" } );
+    $template->param( locale => loc_code(), graphs => \@graphs );
+
+    print $cgi->header( -type => 'text/html', -charset => 'utf-8' );
+    print $template->output();
 }
 
 # Fetch data from MySQL table
-# The query must always return 3 values, a code for each value, a name for each value, 
+# The query must always return 3 values, a code for each value, a name for each value,
 # and the corresponding numerical value, in that order
 sub fetch {
-	my $query = shift;
+    my $query = shift;
 
-	my @list;
-	my $sql = $dbh->prepare( $query );
-	$sql->execute();
+    my @list;
+    my $sql = $dbh->prepare($query);
+    $sql->execute();
 
-	while(my @row = $sql->fetchrow_array) {
-		push @list, {key => $row[0], name => $row[1], count => $row[2]};
-	}
-	
-	return \@list;	
+    while ( my @row = $sql->fetchrow_array ) {
+        push @list, { key => $row[0], name => $row[1], count => $row[2] };
+    }
+
+    return \@list;
 }
 
 # Fixes datasets where the x-axis are weekdays.
 # It does this in two ways : ensures all days are present even if the "count" is 0, and
 # translates the dayname to the current locale
 sub weekday_fixer {
-	my @list = @_;
+    my @list = @_;
 
-	my %dayname = (
-		'en' => ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sathurday'],
-		'fr' => ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
-	);
+    my %dayname = (
+        en => [ 'Sunday',   'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',   'Sathurday' ],
+        fr => [ 'Dimanche', 'Lundi',  'Mardi',   'Mercredi',  'Jeudi',    'Vendredi', 'Samedi' ]
+    );
 
-	for my $i (0..6) {
-		if ($list[$i]{key} != $i + 1) {
-			splice @list, $i, 0, {key => $i + 1, name => $dayname{substr($locale, 0, 2)}[$i], count => 0};
-		}
-		else {
-			$list[$i]{name} = $dayname{substr($locale, 0, 2)}[$i];
-		}
-	}
-	return \@list;
+    for my $i ( 0 .. 6 ) {
+
+        # Add missing keys
+        if ( $list[$i]{key} != $i + 1 ) {
+            splice @list, $i, 0, { key => $i + 1, name => $dayname{ loc_code() }[$i], count => 0 };
+        }
+
+        # Translate existing keys
+        else {
+            $list[$i]{name} = $dayname{ loc_code() }[$i];
+        }
+    }
+    return \@list;
+}
+
+sub loc_code {
+    my $str = substr( $locale, 0, 2 );
+
+    for my $loc (@supportedLocales) {
+        if ( $loc eq $str ) {
+            return $str;
+        }
+    }
+
+    $str = 'en';
+    return $str;
 }
 
 #Supprimer le plugin avec toutes ses données
 sub uninstall() {
-	my ( $self, $args ) = @_;
-	my $table = $self->get_qualified_table_name('mytable');
+    my ( $self, $args ) = @_;
+    my $table = $self->get_qualified_table_name('mytable');
 
-	return C4::Context->dbh->do("DROP TABLE $table");
+    return C4::Context->dbh->do("DROP TABLE $table");
 }
 
 1;
