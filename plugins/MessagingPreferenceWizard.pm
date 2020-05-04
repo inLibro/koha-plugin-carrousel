0329a0961414627763331b726c1c33a6932a9f18
Blou <blou@inlibro.com>
2020-05-04 13:41:37 -0400

    ajout de l'entête

--- plugins/MessagingPreferenceWizard.pm
+++ plugins/MessagingPreferenceWizard.pm
@@ -1,5 +1,24 @@
 package Koha::Plugin::MessagingPreferenceWizard;
-
+# Bouzid Fergani, 2016 - InLibro
+#
+# This plugin allows you to generate a Carrousel of books from available lists
+# and insert the template into the table system preferences;OpacMainUserBlock
+#
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 use Modern::Perl;
 
